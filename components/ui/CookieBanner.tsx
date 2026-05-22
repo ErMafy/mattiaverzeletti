9bf0471636910561c16368dbd9a45d9c6172b4a9
TheWolf0105 <ayoubmahfoud6@gmail.com>
2026-05-22 14:34:44 +0200

    aggiunta ultime cosase

--- components/ui/CookieBanner.tsx
+++ components/ui/CookieBanner.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
 
 export default function CookieBanner() {
-  const [isVisible, setIsVisible] = setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const [showPreferences, setShowPreferences] = useState(false);
 
   const [preferences, setPreferences] = useState({
